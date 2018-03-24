cad1a6030156cfd189909461dbeb389fffa9976b
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 17:56:03 +0900

    関数練習①

--- C#_practice2/Assets/Practice.cs
+++ C#_practice2/Assets/Practice.cs
@@ -10,7 +10,8 @@ public class Practice : MonoBehaviour
     
 	// Use this for initialization
 	void Start () {
-        hello();  //関数の呼び出し
+        string name = "アラカワ";  //変数nameの宣言と代入
+        hello(name);  //関数の呼び出し
 	}
 	
 	// Update is called once per frame
@@ -18,8 +19,9 @@ public class Practice : MonoBehaviour
     {
 		
 	}
-    void hello()
+    void hello(string name)  //関数hello
     {
-        print("hello world");
+        string words = name + "さん，こんにちは";
+        print(words);
     }
 }
