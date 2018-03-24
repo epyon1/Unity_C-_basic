7b7bd13895093a46b1e94d5aab554ec1129b029f
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 13:48:31 +0900

    if構文の練習

--- C#_practice2/Assets/Practice.cs
+++ C#_practice2/Assets/Practice.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Practice : MonoBehaviour {
+    public Text text;
 
 	// Use this for initialization
 	void Start () {
+        int num = 3;
+        if (num > 1)
+        {
+            text.text = "1より大きいです";
+        }
 		
 	}
 	
