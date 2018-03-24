cd087dd2c20eaf42c1e7d4220deb5c2fa72649f8
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 17:25:19 +0900

    while文の練習

--- C#_practice2/Assets/Practice.cs
+++ C#_practice2/Assets/Practice.cs
@@ -3,46 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Practice : MonoBehaviour {
+public class Practice : MonoBehaviour
+{
     public Text text;
 
-    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
-
+    
 	// Use this for initialization
 	void Start () {
-        Days day = Days.Monday;  //DaysからMondayを取得し，変数dayに代入
-        switch (day)
+        int num = 0;
+        while (num <= 10)
         {
-            case Days.Sunday:
-                text.text = "日曜日です";
-                break;
-            case Days.Monday:
-                text.text = "月曜日です";
-                break;
-            case Days.Tuesday:
-                text.text = "火曜日です";
-                break;
-            case Days.Wednesday:
-                text.text = "水曜日です";
-                break;
-            case Days.Thursday:
-                text.text = "木曜日です";
-                break;
-            case Days.Friday:
-                text.text = "金曜日です";
-                break;
-            case Days.Saturday:
-                text.text = "土曜日です";
-                break;
-            default:
-                text.text = "どの曜日でもありません";
-                break;
+            print (num);
+            num += 1 ;
         }
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
