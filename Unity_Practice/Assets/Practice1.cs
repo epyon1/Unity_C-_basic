8635559f10ac2a4b3c570c10823212e4fcee7b50
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 20:36:45 +0900

    基礎文法問題１

---
+++ Unity_Practice/Assets/Practice1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string name = "kento";
+        int birthMonth = 6;
+        int birthDay = 9;
+        int Sum = birthMonth += birthDay;
+        //Sum(birthMonth, birthDay);
+        print("私の名前は" + name + "です．生まれた月の数字と生まれた日にちを足すと" + Sum +"になります．");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //int Sum(int a, int b)
+    //    int a = birthMonth;
+    //int b = birthDay;
+    //    return a + b ;
+}
