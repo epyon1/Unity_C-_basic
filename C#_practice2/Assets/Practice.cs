3ca2345fd9593d91a42293cc7d782e3b0003ecb1
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 17:15:28 +0900

    enum列挙型を使って，switch構文の練習

--- C#_practice2/Assets/Practice.cs
+++ C#_practice2/Assets/Practice.cs
@@ -6,18 +6,37 @@ using UnityEngine.UI;
 public class Practice : MonoBehaviour {
     public Text text;
 
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
 	// Use this for initialization
 	void Start () {
-        int num = 0;
-        if (num > 0)
+        Days day = Days.Monday;  //DaysからMondayを取得し，変数dayに代入
+        switch (day)
         {
-            text.text = "値は正です";
-        }else if(num==0){
-            text.text = "値は0です";
-        }
-        else
-        {
-            text.text = "値は負です";
+            case Days.Sunday:
+                text.text = "日曜日です";
+                break;
+            case Days.Monday:
+                text.text = "月曜日です";
+                break;
+            case Days.Tuesday:
+                text.text = "火曜日です";
+                break;
+            case Days.Wednesday:
+                text.text = "水曜日です";
+                break;
+            case Days.Thursday:
+                text.text = "木曜日です";
+                break;
+            case Days.Friday:
+                text.text = "金曜日です";
+                break;
+            case Days.Saturday:
+                text.text = "土曜日です";
+                break;
+            default:
+                text.text = "どの曜日でもありません";
+                break;
         }
 		
 	}
