acb6f13a80658013092b00fc1627c7f690d065ab
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 19:30:33 +0900

    クラスとインスタンスについて学習

--- C#_practice3/Assets/Sample.cs
+++ C#_practice3/Assets/Sample.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  //このパッケージをいれることで、Canvas上のTextなどをプログラムで使用できるようになる
 
 public class Sample : MonoBehaviour {
+    public Text text;  //Textオブジェクトが代入される変数textの宣言
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        string str = "Hello World!";
+        ShowMessage(str);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    void ShowMessage (string s)
+    {
+        text.text = s;  //実際に文字を表示させる
+    }
 }
