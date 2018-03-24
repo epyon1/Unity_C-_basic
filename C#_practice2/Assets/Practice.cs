e20591a8a6c701e6cb23f995bdbd30ae72462127
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-24 18:09:20 +0900

    関数の練習②(引数を使った関数)

--- C#_practice2/Assets/Practice.cs
+++ C#_practice2/Assets/Practice.cs
@@ -11,7 +11,8 @@ public class Practice : MonoBehaviour
 	// Use this for initialization
 	void Start () {
         string name = "アラカワ";  //変数nameの宣言と代入
-        hello(name);  //関数の呼び出し
+        hello(name);  //関数helloを呼び出す．引数はname
+        greet(17);  //greet関数の呼び出し
 	}
 	
 	// Update is called once per frame
@@ -22,6 +23,22 @@ public class Practice : MonoBehaviour
     void hello(string name)  //関数hello
     {
         string words = name + "さん，こんにちは";
-        print(words);
+        print(words);  //アラカワさんこんにちは
+    }
+    void greet(int hour)
+    {
+        if (hour >= 5 && hour <= 10)
+        {
+            print("おはようございます");
+        }
+        else if (hour >= 11 && hour <= 17)
+        {
+            print("こんにちは");
+        }
+        else
+        {
+            print("こんばんは");
+        }
+
     }
 }
