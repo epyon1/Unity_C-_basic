aad7c8ac2d809340aee54407d5ac1262623741d0
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-27 19:35:40 +0900

    基礎文法問題２

---
+++ Unity_Practice/Assets/Practice2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print(1869/456/2);
+        print(5974%407);
+        print((78 + 450) * (4 + 36));
+        print((5124%9)*(354-28));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
