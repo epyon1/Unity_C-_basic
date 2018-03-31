979b99b3bce41088c002af6327fe78789fb605d9
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-31 23:48:09 +0900

    基礎文法問題５

---
+++ Unity_Practice/Assets/Practice５.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice５ : MonoBehaviour {
+    int num = 100;
+
+	// Use this for initialization
+	void Start () {
+	while(num >= 1)
+        {
+            Debug.Log(num);
+            num -=1;
+
+
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
