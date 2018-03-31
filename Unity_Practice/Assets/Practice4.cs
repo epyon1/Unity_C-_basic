47a0f94db6000ab9a9cb4b38f299014a931a85e5
epyon <33213339+epyon1@users.noreply.github.com>
2018-03-31 23:41:47 +0900

    基礎文法問題４

---
+++ Unity_Practice/Assets/Practice4.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice4 : MonoBehaviour {
+    public string name ;
+
+	// Use this for initialization
+	void Start () {
+        Choice(name);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Choice(string n)
+    {
+        switch (n)
+        {
+            case "山田":
+                Debug.Log("私は山田という人を知っています");
+                break;
+            case "松下":
+                Debug.Log("私は松下という人を知っています");
+                break;
+            default:
+                Debug.Log("私はその人を知りません");
+                break;
+        }
+    }   
+}
