9fd6cfb088a5051307920fc0ba5a7af7d3c916ff
epyon <33213339+epyon1@users.noreply.github.com>
2018-04-01 14:46:03 +0900

    基礎文法問題６

---
+++ Unity_Practice/Assets/Practice6.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice6 : MonoBehaviour {
+
+    public int sum;
+    //public int i;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Debug.Log(i);
+            sum += i;
+        }
+        Debug.Log(sum);
+
+            ;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
